708e5789da4a4b9e734b5f23d0eefa736653a0d8
LimLan <sm.m.panoramik@gmail.com>
2020-05-17 10:39:16 +0300

    init commit

---
+++ Assets/Scripts/MouseLock.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLock : MonoBehaviour
+{
+    public float mouseSens = 100f;
+
+    public Transform playerBody;
+
+    private float xRotation;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var realMouseSens = mouseSens * Time.deltaTime;
+        var mouseX = Input.GetAxis("Mouse X") * realMouseSens;
+        var mouseY = Input.GetAxis("Mouse Y") * realMouseSens;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+    
+    void OnApplicationFocus(bool hasFocus)
+    {
+        xRotation = 0;
+        transform.localRotation = Quaternion.Euler(0, 0f, 0f);
+        playerBody.transform.localRotation = Quaternion.Euler(0, 0f, 0f);
+    }
+
+    void OnApplicationPause(bool pauseStatus)
+    {
+        xRotation = 0;
+        transform.localRotation = Quaternion.Euler(0, 0f, 0f);
+        playerBody.transform.localRotation = Quaternion.Euler(0, 0f, 0f);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerMovement : MonoBehaviour
+    {
+        public CharacterController controller;
+        public Transform playerBody;
+
+        public float speed = 12f;
+        public float gravity = -9.81f;
+        public float jumpHeight = 10f;
+
+        public Transform groundCheck;
+        public float groundDistance = 0.4f;
+        public LayerMask groundMask;
+        
+        private Vector3 velocity;
+        private bool isGrounded;
+
+        private void Update()
+        {
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+
+            Vector3 move = transform.right * x + transform.forward * z;
+            
+            controller.Move(playerBody.rotation * move * (speed * Time.deltaTime));
+
+            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            }
+
+            velocity.y += gravity * Time.deltaTime;
+            
+            controller.Move(velocity * Time.deltaTime);
+        }
+
+    }
+}
