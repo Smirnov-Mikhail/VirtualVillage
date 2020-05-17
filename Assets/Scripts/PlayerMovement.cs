44523e7cd7ca51f11d8d19b4143dbc6caf70cbb6
LimLan <sm.m.panoramik@gmail.com>
2020-05-17 18:51:57 +0300

    add tutorial and make two cameras

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,47 +1,65 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
-namespace DefaultNamespace
+public class PlayerMovement : MonoBehaviour
 {
-    public class PlayerMovement : MonoBehaviour
-    {
-        public CharacterController controller;
-        public Transform playerBody;
-
-        public float speed = 12f;
-        public float gravity = -9.81f;
-        public float jumpHeight = 10f;
-
-        public Transform groundCheck;
-        public float groundDistance = 0.4f;
-        public LayerMask groundMask;
+    [SerializeField] private List<GameObject> houses;
         
-        private Vector3 velocity;
-        private bool isGrounded;
-
-        private void Update()
+    private void Awake()
+    {
+#if UNITY_WEBGL
+        int pm = Application.absoluteURL.IndexOf("?", StringComparison.Ordinal);
+        if (pm != -1)
         {
-            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
-
-            if (isGrounded && velocity.y < 0)
+            var houseName = Application.absoluteURL.Split("?"[0])[1];
+            var house = houses.FirstOrDefault(h => h.name == houseName);
+            if (house != null)
             {
-                velocity.y = -2f;
+                transform.position = house.transform.position;
             }
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
+        }         
+#endif
+    }
 
-            Vector3 move = transform.right * x + transform.forward * z;
+    public CharacterController controller;
+    public Transform playerBody;
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 10f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+        
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
             
-            controller.Move(playerBody.rotation * move * (speed * Time.deltaTime));
+        controller.Move(playerBody.rotation * move * (speed * Time.deltaTime));
 
-            if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-            {
-                velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
-            }
-
-            velocity.y += gravity * Time.deltaTime;
-            
-            controller.Move(velocity * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
+        velocity.y += gravity * Time.deltaTime;
+            
+        controller.Move(velocity * Time.deltaTime);
     }
+
 }
---
+++ Assets/Scripts/TutorialWindow.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UnityEngine;
+
+public class TutorialWindow : MonoBehaviour
+{
+    [SerializeField] private PlayerMovement movementScript;
+    [SerializeField] private MouseLock mouseScript;
+    [SerializeField] private GameObject tutorialVisual;
+
+    private const string TutorSeen = "TutorSeen";
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey(TutorSeen))
+        {
+            StartView();
+        }
+    }
+
+    public void StartView()
+    {
+        movementScript.enabled = true;
+        mouseScript.enabled = true;
+        tutorialVisual.SetActive(false);
+        if (!PlayerPrefs.HasKey(TutorSeen))
+        {
+            PlayerPrefs.SetInt(TutorSeen, 1);
+        }
+    }
+}
